2cc6f332b2525d33d457962f490043ab677a659b
Prashanth Kumar <prash8669@yahoo.com>
2026-01-16 14:39:53 +0530

    Add Tauri + SvelteKit template

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { Navbar, NavBrand, NavHamburger, NavUl, NavLi, Button } from 'flowbite-svelte';
+</script>
+
+<Navbar class="sticky h-10 w-full bg-red-300 dark:bg-gray-900">
+	<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+	<div class="flex items-center lg:order-2">
+		<Button href="/" color="dark">Log in</Button>
+		<Button href="/" class="ml-2" color="red">Get started</Button>
+		<NavHamburger />
+	</div>
+	<NavLi href="/">Home</NavLi>
+	<NavLi href="/">Company</NavLi>
+	<NavLi href="/">Marketplace</NavLi>
+	<NavLi href="/">Features</NavLi>
+	<NavLi href="/">Team</NavLi>
+	<NavLi href="/">Contact</NavLi>
+</Navbar>
